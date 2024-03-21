42c7934cdc87544e69054d430961044e5152a731
Nihar Lekhade <arthurexcalibur9@protonmail.com>
2024-03-21 14:34:54 +0530

    Layout changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,54 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const pages = document.querySelectorAll('.page');
-  let currentPage = 0;
-  let lastScrollTop = 0;
-  let isScrolling = false;
-
-  window.addEventListener('wheel', function (e) {
-    if (!isScrolling) {
-      isScrolling = true;
-      const st = window.scrollY || document.documentElement.scrollTop;
-      if (st > lastScrollTop) {
-        // Scrolling down
-        if (currentPage < pages.length - 1) {
-          currentPage++;
-          scrollToPage(currentPage);
-        }
-      } else {
-        // Scrolling up
-        if (currentPage > 0) {
-          currentPage--;
-          scrollToPage(currentPage);
-        }
-      }
-      lastScrollTop = st <= 0 ? 0 : st;
-      e.preventDefault();
-    }
+  document.getElementById('about-button').addEventListener('click', function () {
+    var contentElement = document.getElementById('about');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
   });
 
-  function scrollToPage(index) {
-    pages[index].scrollIntoView({ behavior: 'smooth', block: 'start' });
-    setTimeout(() => {
-      isScrolling = false;
-    }, 800); // Adjust scroll duration as needed
-  }
-
-
-  const aboutButton = document.getElementById("about-button");
-  aboutButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    scrollToPage(0);
+  document.getElementById('projects-button').addEventListener('click', function () {
+    var contentElement = document.getElementById('projects');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
   });
 
-  const projectsButton = document.getElementById("projects-button");
-  projectsButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    scrollToPage(1);
-  });
-
-  const workButton = document.getElementById("work-button");
-  workButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    scrollToPage(3);
+  document.getElementById('work-button').addEventListener('click', function () {
+    var contentElement = document.getElementById('work');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
   });
 });
